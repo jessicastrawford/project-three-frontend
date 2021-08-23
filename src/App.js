9a9c58ebf1636633480b146b6ccad45d3db0a791
Jessica Strawford <jessicastrawford@gmail.com>
2021-08-23 12:42:26 +0100

    slideshow

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Nav from './components/common/Navbar'
+import Navbar from './components/common/Navbar'
 import Home from './components/common/Home'
 import ClubsIndex from './components/clubs/ClubsIndex'
 import Login from './auth/Login'
 import Register from './auth/Register'
 import InteractiveMap from './components/maps/InteractiveMap'
+// import './App.scss'
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/components/common/Loading.js
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <section className="box">
+      <div className="shadow"></div>
+      <div className="gravity">
+        <div className="ball"></div>
+      </div>
+    </section>
+  )
+}
+
+export default Loading
