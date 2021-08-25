676d776d9ece7bf52e2d76aff727a992b9ac82ba
Jessica Strawford <jessicastrawford@gmail.com>
2021-08-25 14:14:36 +0100

    adding a show page for the clubs

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,6 +8,8 @@ function headers() {
 }
 
 
+const baseUrl = '/api'
+
 // Auth Requests
 export function registerUser(formData) {
   return axios.post('api/register', formData)
@@ -21,6 +23,30 @@ export function showUserProfile() {
   return axios.get('api/profile/', headers())
 }
 
+
+// Club Requests
+
+export function getAllClubs() {
+  return axios.get('/api/clubs')
+}
+
+export function getSingleClub(clubId) {
+  return axios.get(`${baseUrl}/clubs/${clubId}`)
+}
+
+export function createClub(formData) {
+  return axios.post('/api/clubs', formData, headers())
+}
+
+export function deleteClub(clubId, formData) {
+  return axios.delete(`/api/clubs/${clubId}`, formData, headers())
+}
+
+export function editClub(clubId, formData) {
+  return axios.put(`/api/clubs/${clubId}`, formData, headers())
+}
+
+
 // Pub Requests
 
 
@@ -29,7 +55,7 @@ export function getAllPubs() {
 }
 
 export function getSinglePub(pubId) {
-  return axios.get(`/api/pubs/${pubId}`)
+  return axios.get(`/pubs/${pubId}`)
 }
 
 export function createPub(formdata) {
@@ -43,3 +69,6 @@ export function deletePub(pubId) {
 export function editPub(pubId, formdata){
   return axios.put(`/api/pubs/${pubId}`, formdata, headers())
 }
+
+// Comments
+
